08c963802cd53bb524d90cda18e16f64e2e857c9
AlperKaanYildirim <alperyildirim@memorial.com.tr>
2024-10-22 17:26:13 +0300

    vacuum sorgusu eklendi.

---
+++ Vacuum.sql
@@ -0,0 +1,255 @@
+
+
+--------- Protokol -------
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201801";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201802";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201803";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201804";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201805";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201806";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201807";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201808";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201809";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201810";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201811";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201812";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201901";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201902";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201903";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201904";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201905";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201906";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201907";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201908";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201909";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201910";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201911";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P201912";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202001";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202002";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202003";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202004";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202005";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202006";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202007";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202008";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202009";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202010";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202011";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202012";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202101";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202102";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202103";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202104";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202105";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202106";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202107";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202108";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202109";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202110";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202111";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202112";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202201";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202202";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202203";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202204";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202205";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202206";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202207";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202208";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202209";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202210";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202211";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202212";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202301";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202302";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202303";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202304";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202305";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202306";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202307";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202308";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202309";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202310";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202311";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202312";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202401";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202402";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202403";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202404";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202405";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202406";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202407";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202408";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202409";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTProtokol_P202410";
+
+ -------- Fatura --------
+
+/* 
+
+vacuum(full,analyze,verbose) "MEMOBI_DM"."DM_FaturaRaporTablosuDetay"; 
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202301";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202302";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202303";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202304";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202305";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202306";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202307";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202308";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202309";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202310";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202311";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202312";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202401";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202402";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202403";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202404";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202405";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202406";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202407";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202408";
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202409"
+
+vacuum (full,analyze,verbose) "MEMOBI_DWH"."FCTFatura_P202410"
+
+------- Tahsilar -----------
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTTahsilat_P202401";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTTahsilat_P202402";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTTahsilat_P202403";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTTahsilat_P202404";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTTahsilat_P202405";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTTahsilat_P202406";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTTahsilat_P202407";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTTahsilat_P202408";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTTahsilat_P202409";
+
+vacuum(full,analyze,verbose)  "MEMOBI_DWH"."FCTTahsilat_P202410";
+
+------- ODS --------
+
+vacuum (full,analyze,verbose) "MEMOBI_ODS_HBYS"."Finans_Fatura" ;
+
+vacuum (full,analyze,verbose) "MEMOBI_ODS_HBYS"."Finans_FaturaKalem" ;
+
+vacuum (full,analyze,verbose) "MEMOBI_ODS_HBYS"."Hasta_ProtokolIslemTutar";
+
+vacuum (full,analyze,verbose) "MEMOBI_ODS_HBYS"."Hasta_ProtokolIslem";
+
+vacuum (full,analyze,verbose) "MEMOBI_ODS_HBYS"."Hasta_Protokol";
+
+vacuum (full,analyze,verbose) "MEMOBI_ODS_HBYS"."Finans_VezneIslem";
+
+vacuum (full,analyze,verbose) "MEMOBI_ODS_HBYS"."Finans_VezneIslemKalem"; */
